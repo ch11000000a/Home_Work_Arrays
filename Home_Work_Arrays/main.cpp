f77439eac914d88eb6b5736e6c2a0e7380878de4
ch11000000a <1100000011010111@mail.ru>
2022-10-20 21:49:11 +0700

    Add project files.

---
+++ Home_Work_Arrays/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+//#define SHIFT_LEFT
+#define SHIFT_RIGHT
+void main() {
+	setlocale(LC_ALL, "");
+	const int size = 10;
+	int arr[size];
+	int arr_save[1];
+	int shift;
+	cout << "¬ведите массив: ";
+	for (int i = 0; i < size; i++)
+	{
+		cin >> arr[i];
+	}
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+#ifdef SHIFT_LEFT
+	cout << "¬ведите на слько элементов сдвинуть массив влево: "; cin >> shift;
+	for (int i = 0; i < shift; i++)
+	{
+		arr_save[0] = arr[0];
+		for (int i = 0; i < size; i++)
+		{
+			arr[i] = arr[(i + 1)];
+		}
+		arr[(size - 1)] = arr_save[0];
+	}
+#endif // SHIFT_LEFT
+#ifdef SHIFT_RIGHT
+	cout << "¬ведите на слько элементов сдвинуть массив вправо: "; cin >> shift;
+	for (int i = 0; i < shift; i++)
+	{
+		arr_save[0] = arr[(size - 1)];
+		for (int i = (size - 1); i >= 0; i--)
+		{
+			arr[i] = arr[(i - 1)];
+		}
+		arr[0] = arr_save[0];
+	}
+#endif // SHIFT_RIGHT
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
